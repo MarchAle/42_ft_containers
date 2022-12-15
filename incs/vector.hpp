1b8c511115821eba6ba338ae8ab88c0367617962
Alexi Marchal <amarchal@z4r3p1.42lyon.fr>
2022-12-15 17:03:35 +0100

    copy constr to do

--- incs/vector.hpp
+++ incs/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: amarchal <amarchal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 11:32:39 by amarchal          #+#    #+#             */
-/*   Updated: 2022/12/14 17:07:25 by amarchal         ###   ########.fr       */
+/*   Updated: 2022/12/15 16:54:16 by amarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,28 @@ namespace ft
 	template<typename T, class Allocator = std::allocator<T> > 
 	class vector
 	{
-		// typedef typename std::size_t size_type;
+		typedef T value_type;
 
 		private:
 			T*			array;
 			Allocator 	alloc;
 			size_t		arr_size;
 			size_t		vec_capacity;
+			
+			void	_move_array()
+			{
+				T*		new_array;
+				
+				vec_capacity *= 2;
+				new_array = alloc.allocate(vec_capacity);
+				for (size_t i = 0; i < arr_size; i++)
+				{
+					alloc.construct(new_array + i, array[i]);
+					alloc.destroy(array + i);
+				}
+				alloc.deallocate(array, vec_capacity / 2);
+				array = new_array;
+			};
 			
 		public:
 			vector()
@@ -39,9 +54,23 @@ namespace ft
 				arr_size = 0;
 				vec_capacity = 1;
 			};
+			vector(const vector &src)
+			{
+				std::cout << "copy constr called" << std::endl;
+				*this = src;
+			};
+			vector &operator=(const vector &src)
+			{
+				this->array = src.array;
+				this->arr_size = src.arr_size;
+				this->vec_capacity = src.vec_capacity;
+				this->alloc = src.alloc;
+				
+				return (*this);
+			};
 			~vector()
 			{
-				alloc.deallocate(array, arr_size);
+				// alloc.deallocate(array, vec_capacity);
 			};
 
 			///////// MODIFIERS
@@ -50,23 +79,12 @@ namespace ft
 				if (arr_size == 0)
 					array = alloc.allocate(vec_capacity);
 				else if (arr_size + 1 > vec_capacity)
-					move_array();
-				array[arr_size] = e;
+					_move_array();
+				alloc.construct(array + arr_size, e);
+				// array[arr_size] = e;
 				arr_size++;
 			};
 
-			void	move_array()
-			{
-				T*		new_array;
-				
-				vec_capacity *= 2;
-				new_array = alloc.allocate(vec_capacity);
-				for (size_t i = 0; i < arr_size; i++)
-					new_array[i] = array[i];
-				alloc.deallocate(array, vec_capacity / 2);
-				array = new_array;
-			};
-			
 			///////// CAPACITY
 			size_t	size() const
 			{
@@ -80,32 +98,119 @@ namespace ft
 
 			void	resize(size_t n)
 			{
+				T*			new_array;
+				T			test;
+				size_t		i = 0;
+				
+				if (n > max_size())
+					throw std::out_of_range("Can't allocate");
+				if (n == arr_size)
+					return ;
+				if (n < vec_capacity)
+				{
+					new_array = alloc.allocate(vec_capacity);
+					while (i < arr_size && i < n)
+					{
+						new_array[i] = array[i];
+						i++;
+					}
+					while (i < n)
+					{
+						new_array[i] = test;
+						i++;
+					}
+					alloc.deallocate(array, vec_capacity);
+				}
+				else
+				{
+					size_t old_capacity = vec_capacity;
+					while (vec_capacity < n && vec_capacity < max_size())
+						vec_capacity *= 2;
+					try
+					{
+						new_array = alloc.allocate(vec_capacity);
+					}
+					catch(const std::exception& e)
+					{
+						std::cerr << e.what() << '\n';
+						return ;
+					}
+					while (i < arr_size && i < n)
+					{
+						new_array[i] = array[i];
+						i++;
+					}
+					while (i < n)
+					{
+						new_array[i] = test;
+						i++;
+					}
+					alloc.deallocate(array, old_capacity);
+				}
+				array = new_array;
+				arr_size = n;
+			};
+				
+			void	resize(size_t n, T val)
+			{
 				T*		new_array;
 				size_t	i = 0;
 				
+				if (n > max_size())
+					throw std::out_of_range("Can't allocate");
 				if (n == arr_size)
 					return ;
-				new_array = alloc.allocate(n);
-				while (i < arr_size && i < n)
+				if (n < vec_capacity)
 				{
-					new_array[i] = array[i];
-					i++;
+					new_array = alloc.allocate(vec_capacity);
+					while (i < arr_size && i < n)
+					{
+						alloc.construct(new_array + i, array[i]);
+						alloc.destroy(array + i);
+						// new_array[i] = array[i];
+						i++;
+					}
+					while (i < n)
+					{
+						alloc.construct(new_array + i, val);
+						// new_array[i] = val;
+						i++;
+					}
+					alloc.deallocate(array, vec_capacity);
 				}
-				while (i < n)
+				else
 				{
-					new_array[i] = 0;
-					i++;
+					size_t old_capacity = vec_capacity;
+					while (vec_capacity < n && vec_capacity < max_size())
+						vec_capacity *= 2;
+					try
+					{
+						new_array = alloc.allocate(vec_capacity);
+					}
+					catch(const std::exception& e)
+					{
+						std::cerr << e.what() << '\n';
+						return ;
+					}
+					while (i < arr_size && i < n)
+					{
+						alloc.construct(new_array + i, array[i]);
+						alloc.destroy(array + i);
+						// new_array[i] = array[i];
+						i++;
+					}
+					while (i < n)
+					{
+						alloc.construct(new_array + i, val);
+						// new_array[i] = val;
+						// std::cout << " Ici " << (int)val[0] << std::endl;
+						i++;
+					}
+					alloc.deallocate(array, old_capacity);
 				}
-				alloc.deallocate(array, vec_capacity);		
 				array = new_array;
 				arr_size = n;
-				vec_capacity = n;
 			};
-			
-			// void	resize(size_t n, value_type val = value_type())
-			// {
-				
-			// };
 			
 			size_t	capacity() const
 			{
@@ -135,7 +240,7 @@ namespace ft
 				return (array[index]);	
 			};
 
-			T &front()
+			T& front()
 			{
 				return (array[0]);
 			};
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amarchal <amarchal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 14:08:50 by amarchal          #+#    #+#             */
-/*   Updated: 2022/12/14 17:07:08 by amarchal         ###   ########.fr       */
+/*   Updated: 2022/12/15 16:47:09 by amarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,54 @@
 
 int	main()
 {
-	std::vector<int> vec;
-	ft::vector<int> my_vec;
+	// std::vector<std::string> my_vec;
+	// std::vector<std::vector<int> > my_vec;
+	std::vector<ft::vector<int> > my_vec;
 
 	// vec.push_back(0);
 	// std::cout << vec[6] << std::endl;
 	
 	int array_size = 5;
-	for (int i = 1; i < array_size + 1; i++)
-		my_vec.push_back(i);
-
-	// try
-	// {
-	// 	// my_vec.at(189) = 88;
-	// 	my_vec.at(0) = 456;
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cerr << e.what() << '\n';
-	// }
-	std::cout << "Size : " << my_vec.size() << " Capacity : " << my_vec.capacity() << std::endl;
-	my_vec.resize(11);
-	std::cout << "Size : " << my_vec.size() << " Capacity : " << my_vec.capacity() << std::endl;
-	my_vec.push_back(666);
-	std::cout << "Size : " << my_vec.size() << " Capacity : " << my_vec.capacity() << std::endl;
-	// my_vec.push_back(666);
-	// my_vec.push_back(666);
+	for (int i = 0; i < array_size; i++)
+	{
+		// std::vector<int> test;
+		ft::vector<int> test;
+		test.push_back(123);
+		test.push_back(789);
+		my_vec.push_back(test);
+		// std::cout << "Size : " << my_vec.size() << " Capacity : " << my_vec.capacity() << std::endl;
+		// std::cout << "test " << test.front() << " " << test.back() << std::endl;
+		// std::cout << "my_vec.at(i)\t" << my_vec.at(i).front() << " " << my_vec.at(i).back() << std::endl;
+		// std::cout << "my_vec[i]\t" << my_vec[i].front() << " " << my_vec[i].back() << std::endl;
+	}
+	try
+	{
+		// my_vec.at(189) = 88;
+		// my_vec.at(0) = 456;
+		// std::cout << "Size : " << my_vec.size() << " Capacity : " << my_vec.capacity() << std::endl;
+		ft::vector<int> blabla;
+		blabla.push_back(333);
+		my_vec.resize(9, blabla);
+		blabla[0] = 111;
+		// std::cout << "Size : " << my_vec.size() << " Capacity : " << my_vec.capacity() << std::endl;
+		// my_vec.push_back("coucou");
+		std::cout << "Size : " << my_vec.size() << " Capacity : " << my_vec.capacity() << std::endl;
+		// std::cout << "Size : " << my_vec[0].front() << " Capacity : " << my_vec[0].capacity() << std::endl;
+		// my_vec.push_back(666);
+		// my_vec.push_back(666);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 		
 	for (size_t i = 0; i < my_vec.size(); i++)
-		std::cout << my_vec[i] << " " << my_vec.at(i) << std::endl;
+	{
+		std::cout << "print my_vec[i]\t" << my_vec[i].front() << " " << my_vec[i].back() << std::endl;
+		// std::cout << my_vec[i].front() << " " << my_vec.at(i).back() << std::endl;
+	}
 	
 	// std::cout << "Max size : " << my_vec.max_size() << std::endl;
-	std::cout << "\nFront : " << my_vec.front() << std::endl;
-	std::cout << "Back : " << my_vec.back() << std::endl;
+	// std::cout << "\nFront : " << my_vec.front().size() << std::endl;
+	// std::cout << "Back : " << my_vec.back().size() << std::endl;
 }
